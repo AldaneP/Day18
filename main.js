b9417ff16f969c2af80e7d5cdd60e7833f6dc06b
Aldane Purrier <aldanepur@gmail.com>
2019-05-16 14:26:15 -0400

    Media Queries Lesson

---
+++ main.js
@@ -0,0 +1,40 @@
+//  stack -> LIFO
+// queue -> FIFO
+// tree
+
+// array = ['roy','ike','marth','lucina','corrin']
+//
+// class Stack {
+//   constructor(arr) {
+//     this.list = arr
+//   }
+//
+//   get() {}
+//   let item = this.list.pop()
+//   this.list.pop()
+// }
+//   addItem() {
+//     this.list.push(item)
+//
+//   }
+// }
+//
+//   class Queue {
+//     constructor() {
+//       this.list = arr
+//     }
+//
+//     get() {
+//      return this.list.shift()
+//     }
+//   }
+//
+// let stack = new Stack(array)
+// let queue = new Queue(array)
+
+
+// let Kingdom = {
+//   king: {name:'Roy', knights: {k1: 'Lucina', k2: 'marth'} },
+//   queen: {name: '', children: {prince: '', princess:''} }
+//   }
+// }
